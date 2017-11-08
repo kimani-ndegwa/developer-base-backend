d86620323123f48f04e46991d373aaa42af9cc2e
kimani-ndegwa <kimani.ndegwa@andela.com>
2017-11-08 17:56:07 +0300

    Configure controllers and routes for the skills section

--- controllers/skill.js
+++ controllers/skill.js
@@ -1,7 +1,62 @@
-const Skill = require('../controllers/skill');
+const Skill = require('../models/skill');
 
 const SkillActions = {
     getAllSkills : (req, res)=>{
-        res.send('Get All Developers');
+        let developerId = req.params._id;
+        Skill.find({developer: developerId}).then(skills=>{
+            res.send(skills);
+        }).catch(e=>{
+            console.log(e);
+        })
+    },
+
+    createSingleSkill : (req, res)=>{
+        let developerId = req.params._id;
+        // We dont have to pass the actual developer this skill belongs to
+        req.body.developer = developerId;
+        Skill.create(req.body).then(skill=>{
+            res.send(skill);
+        }).catch(e=>{
+            console.log(e);
+        })
+    },
+
+
+    getSingleSkill : (req, res) =>{
+        console.log('Gets here >>>>>>>.1')
+        let developerId = req.params._id;
+        let skillId = req.params._skillId;
+        console.log('We got here>>>>>>>>>>>>')
+        Skill.find({_id: skillId, developer: developerId }).then(skill=>{
+            console.log('>>>>>>>>>>>>>', skill)
+            res.send(skill)
+        }).catch(e=>{
+            console.log(e);
+        })
+    },
+
+    updateSingleSkill: (req,res) => {
+        let developerId = req.params._id;
+        let skillId = req.params._skillId;
+
+        Skill.findOneAndUpdate({_id: skillId, developer: developerId, new: true}).then(skill=>{
+            res.send(skill);
+        }).catch(e=>{
+            console.log(e);
+        })
+    },
+
+    deleteSingleSkill: (req, res)=>{
+        let developerId = req.params._id;
+        let skillId = req.params._skillId;
+
+        Skill.findOneAndRemove({_id: skillId, developer: developerId}).then(()=>{
+            res.status(204).send({message: 'Skill Deleted'})
+        }).catch(e=>{
+            console.log(e);
+        })
     }
+
 }
+
+module.exports = SkillActions;
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const DeveloperActions = require('../controllers/developer');
+const SkillActions = require('../controllers/skill');
 
 // Include Configuration for cors and bodyparser
 
@@ -14,6 +15,18 @@ Router.get('/developer/:_id', DeveloperActions.getSingleDeveloper);
 Router.put('/developer/:_id', DeveloperActions.updateSingleDeveloper);
 Router.delete('/developer/:_id', DeveloperActions.deleteSingleEmployee);
 Router.post('/developers', DeveloperActions.createDeveloper);
+
+// Initial Routes for the skills.
+
+Router.get('/developer/:_id/skills', SkillActions.getAllSkills);
+Router.post('/developer/:_id/skills', SkillActions.createSingleSkill);
+
+Router.get('/developer/:_id/skill/:_skillId', SkillActions.getSingleSkill);
+
+Router.put('/developer/:_id/skill/:_skillId', SkillActions.updateSingleSkill);
+
+Router.delete('/developer/:_id/skill/:_skillId', SkillActions.deleteSingleSkill);
+
 
 
 
