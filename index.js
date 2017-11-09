a125fd2278910a486fe6b0bd622c4e1afb7fcdfa
kimani-ndegwa <kimani.ndegwa@andela.com>
2017-11-09 10:29:30 +0300

    Change PORT settings for heroku

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 8081;
+const PORT = process.env.PORT || 5000;
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
